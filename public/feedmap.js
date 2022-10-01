f8d51e9fb55ff1f4d0cb9b0aa6cdcadb29385fe1
NickRoccodev11 <nchls.rcc@gmail.com>
2022-10-01 15:19:50 -0400

    removed ky

---
+++ public/feedmap.js
@@ -0,0 +1,10 @@
+// const posts = require("../controllers/posts");
+
+
+let ptag = document.getElementById("thisone")
+
+ptag.addEventListener('click', runFunc)
+
+function runFunc() {
+    console.log("it worked")
+}
